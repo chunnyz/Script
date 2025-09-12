99f77d44a5e8b635714b69b8ec7d49e1d67716a9
chunnyz <chunnyzxd6931@gmail.com>
2025-09-12 20:05:40 +0700

    Update and rename trd(1).lua to 99night.lua

---
+++ 99night.lua
@@ -0,0 +1,11 @@
+setfpscap(15)
+task.wait(5)
+_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
+    "Gambler" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
+}
+_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
+_G.Max_Diamond = 1111 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
+
+_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
+script_key = "DPsnWEAKKdswtlZcYNeXDiUFBPkXsPDg"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
